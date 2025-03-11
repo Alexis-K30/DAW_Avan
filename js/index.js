9033bb43901166b6d436fdb6b5daeb8e18384b37
Daniel <dfquack08@gmail.com>
2025-03-10 18:58:30 -0600

    Menu de movil

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
-//Ambas funciones solo cumplen el funcionamiento de hacer aparecer
-//y desaparecer el menu lateral
-function abrirMenu(){
-    document.getElementById("right-menu").classList.add("open");
-}
-
-function cerrarMenu(){
-    document.getElementById("right-menu").classList.remove("open")
-}
-
 window.onload = init;
 
 function init () {
-
+    
 }

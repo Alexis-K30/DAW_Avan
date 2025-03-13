65dbaa9b1734c42e67ffbffa72e543b9a75db739
Daniel <dfquack08@gmail.com>
2025-03-13 16:37:41 -0600

    Bootstrap local + ajustes mapa

--- js/index.js
+++ js/index.js
@@ -2,10 +2,34 @@ window.onload = init;
 
 let menuVertical = document.getElementById("barraMenu");
 let barraNav = document.getElementById("barraNav");
+const infoCard = document.getElementById('infoCard');
+//const cardImg = document.getElementById('cardImg');
+const cardTitle = document.getElementById('cardTitle');
+const cardDesc = document.getElementById('cardDesc');
 
-function init () {
+function init() {
     ajustarMenu();
     window.addEventListener("resize", ajustarMenu);
+    infoCard.style.display = 'none';
+
+    document.querySelectorAll('.departamento').forEach(depto => {
+        depto.addEventListener('mouseenter', (event) => {
+            const nombre = event.target.getAttribute('data-nombre');
+            const imgSrc = event.target.getAttribute('data-img');
+            const descripcion = event.target.getAttribute('data-descripcion');
+
+            cardTitle.textContent = nombre;
+            //cardImg.src = imgSrc;
+            cardDesc.textContent = descripcion;
+            infoCard.style.display = 'block';
+            const instance = Popper.createPopper(event.target, infoCard, {
+                placement: 'top'
+            });
+        });
+        depto.addEventListener('mouseleave', () => {
+            infoCard.style.display = 'none';
+        });
+    });
 }
 
 function ajustarMenu() {
@@ -13,31 +37,3 @@ function ajustarMenu() {
         menuVertical.style.height = window.innerHeight - barraNav.offsetHeight + "px";
     }
 }
-
-const infoCard = document.getElementById('infoCard');
-//const cardImg = document.getElementById('cardImg');
-const cardTitle = document.getElementById('cardTitle');
-const cardDesc = document.getElementById('cardDesc');
-
-document.querySelectorAll('.departamento').forEach(depto => {
-    depto.addEventListener('mouseenter', (event) => {
-        const nombre = event.target.getAttribute('data-nombre');
-        const imgSrc = event.target.getAttribute('data-img');
-        const descripcion = event.target.getAttribute('data-descripcion');
-
-        cardTitle.textContent = nombre;
-        //cardImg.src = imgSrc;
-        cardDesc.textContent = descripcion;
-        infoCard.style.display = 'block';
-        const instance = Popper.createPopper(event.target, infoCard, {
-            placement: 'top'
-        });
-    });
-    depto.addEventListener('mouseleave', () => {
-        infoCard.style.display = 'none';
-    });
-});
-
-
-document.getElementById('year').textContent = "©"+ new Date().getFullYear();
-

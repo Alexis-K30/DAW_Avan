fed31d77ad6d27dae7080ececa108fb13ef8991f
Daniel <dfquack08@gmail.com>
2025-03-12 18:29:39 -0600

    Ajustes a menú lateral

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 window.onload = init;
 
+let menuVertical = document.getElementById("barraMenu");
+let barraNav = document.getElementById("barraNav");
+
 function init () {
-    
+    ajustarMenu();
+    window.addEventListener("resize", ajustarMenu);
+}
+
+function ajustarMenu() {
+    if (window.innerWidth <= 1000) {
+        menuVertical.style.height = window.innerHeight - barraNav.offsetHeight + "px";
+    }
 }
